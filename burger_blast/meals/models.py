c0435b758f830c861bf692c5b1ac3ac41eb3e248
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-01-09 18:24:54 +0000

    Added slug to meals model

--- burger_blast/meals/models.py
+++ burger_blast/meals/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
 class Meal(models.Model):
     name = models.CharField(max_length=55)
     description = models.TextField(max_length=500)
@@ -11,6 +12,12 @@ class Meal(models.Model):
     calories = models.IntegerField()
     allergens = models.CharField(max_length=55)
     preperation_time = models.IntegerField()
+    slug = models.SlugField(blank=True, null=True)
+    
+    def save(self, *args, **kwargs):
+        if not sel.slug and self.name:
+            self.slug = slugify(self.name)
+        super(Meals, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
