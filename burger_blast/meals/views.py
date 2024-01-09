92df429ed5ac904b26a89292c58d1663ce13cb28
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-01-09 19:36:50 +0000

    Added meal_list and meal_detail to views

--- burger_blast/meals/views.py
+++ burger_blast/meals/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Meal
 
 # Create your views here.
+def meal_list(request):
+    meal_list = Meal.objects.all()
+    
+    context = {
+            'meal_list': meal_list,
+    }
+    
+    render(request, 'meals/meal_list.html', {'meal_list': meal_list})
+
+def meal_detail(request, slug):
+    meal_detail = Meal.objects.get(slug=slug)
+    
+    render(request, 'meals/meal_detail.html', {'meal_detail': meal_detail})
