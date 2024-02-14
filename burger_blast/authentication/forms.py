27a6b699afc1ceca201ae68efd757a19a61a7362
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-14 14:23:44 +0000

    Added Register User Form

---
+++ burger_blast/authentication/forms.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class RegisterUserForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Email'
+            }
+        ),
+    )
+    first_name = forms.CharField(
+        max_length=50,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'First Name'
+            }
+        ),
+    )
+    last_name = forms.CharField(
+        max_length=50,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Last Name'
+            }
+        ),
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "username", "first_name", "last_name", "email",
+            "password1", "password2"
+        )
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
