725a68f7775933de7252c06ab20a15c2ce2fe158
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-14 15:44:56 +0000

    Added delete reservations url

--- burger_blast/reservation/urls.py
+++ burger_blast/reservation/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('make/', make_reservation, name='make_reservation'),
     path('my/', my_reservations, name='my_reservations'),
     path('<int:reservation_id>/edit/', edit_reservation, name='edit_reservation'),
+    path('<int:reservation_id>/delete/', delete_reservation, name='delete_reservation'),
 ]
