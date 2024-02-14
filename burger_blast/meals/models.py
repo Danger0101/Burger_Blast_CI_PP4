3a184293bec92bdd3d5d135010479076b622d9e0
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-14 15:02:05 +0000

    Fix typo in preparation time
    Also modified __str__

--- burger_blast/meals/models.py
+++ burger_blast/meals/models.py
@@ -1,27 +1,16 @@
 from django.db import models
-from django.utils.text import slugify
 
 
 class Meal(models.Model):
     name = models.CharField(max_length=55)
     description = models.TextField(max_length=500)
-    # Add a field for the number of people the meal serves
     people = models.IntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='meals/', null=True, blank=True)
     calories = models.IntegerField()
     allergens = models.CharField(max_length=55)
-    preperation_time = models.IntegerField()
+    preparation_time = models.IntegerField()
     slug = models.SlugField(blank=True, null=True)
-    
-    def save(self, *args, **kwargs):
-        if not sel.slug and self.name:
-            self.slug = slugify(self.name)
-        super(Meals, self).save(*args, **kwargs)
-        
-    class Meta:
-        verbose_name = 'Meal'
-        verbose_name_plural = 'Meals' 
 
     def __str__(self):
-        return self.name
+        return str(self.name)
