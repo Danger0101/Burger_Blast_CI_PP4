a78ecbb4120559a8868fe7ce3aa99c87032f6b2b
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-14 14:20:48 +0000

    Add About View

--- burger_blast/about/views.py
+++ burger_blast/about/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index_view(request):
     context = {'active_page': 'index'}
     return render(request, 'about/index.html', context)
+
+def about_view(request):
+    context = {'active_page': 'about'}
+    return render(request, 'about/about.html', context)
