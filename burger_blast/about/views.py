8e8c044e324a144b4ffe4a551ba1e8bd25e59f50
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-14 14:20:28 +0000

    Add Index View

--- burger_blast/about/views.py
+++ burger_blast/about/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def index_view(request):
+    context = {'active_page': 'index'}
+    return render(request, 'about/index.html', context)
