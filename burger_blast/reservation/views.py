b0637c38bf938466592ef25dd0df225a565c338b
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-14 15:46:40 +0000

    Added my reservation view

--- burger_blast/reservation/views.py
+++ burger_blast/reservation/views.py
@@ -6,3 +6,29 @@ from django.utils import timezone
 from .forms import ReservationForm
 from .models import Reservation
 
+
+@login_required
+def my_reservations(request):
+    if request.user.is_staff:
+        query = request.GET.get('q')
+        if query:
+            reservations = Reservation.objects.filter(
+                Q(user__username__icontains=query) |
+                Q(first_name__icontains=query) |
+                Q(last_name__icontains=query) |
+                Q(reservation_datetime__icontains=query) |
+                Q(confirmation_number__icontains=query)
+            )
+        else:
+            reservations = Reservation.objects.all()
+    else:
+        reservations = Reservation.objects.filter(user=request.user)
+
+    # Convert reservation datetime to the user's timezone
+    for reservation in reservations:
+        reservation.reservation_datetime = timezone.localtime(reservation.reservation_datetime)
+
+    if not reservations:
+        reservations = None  # Set reservations to None if no reservations found
+
+    return render(request, 'reservation/my_reservations.html', {'reservations': reservations})
