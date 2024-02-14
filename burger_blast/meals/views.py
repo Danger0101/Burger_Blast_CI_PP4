63a36994da8e0158d785c95171e0680824ac7ee2
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-14 15:07:55 +0000

    Fresh Start after initial trial
    Removed old meal list as was done as a test case

--- burger_blast/meals/views.py
+++ burger_blast/meals/views.py
@@ -1,17 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseForbidden
 from .models import Meal
 
-# Create your views here.
-def meal_list(request):
-    meal_list = Meal.objects.all()
-    
-    context = {
-            'meal_list': meal_list,
-    }
-    
-    render(request, 'meals/meal_list.html', {'meal_list': meal_list})
-
-def meal_detail(request, slug):
-    meal_detail = Meal.objects.get(slug=slug)
-    
-    render(request, 'meals/meal_detail.html', {'meal_detail': meal_detail})
