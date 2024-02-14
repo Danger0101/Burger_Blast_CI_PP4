9915a9a535dba9e47c8034d95b11b9745d75d608
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-14 14:30:13 +0000

    Added Register User View

--- burger_blast/authentication/views.py
+++ burger_blast/authentication/views.py
@@ -11,3 +11,36 @@ from .forms import (
     CustomUpdateUserForm,
     ChangeUserPasswordForm
 )
+
+
+def register_user(request):
+    """
+    Handle user registration.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: Redirects to the index.
+    """
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, f"{username} Registered successfully")
+            return redirect('about:index')
+        else:
+            messages.error(request, "Invalid form data. "
+                                    "Please check the form.")
+            return render(request, "authentication/register.html", {
+                'form': form
+            })
+    else:
+        form = RegisterUserForm()
+        return render(request, "authentication/register.html", {
+            'form': form
+        })
