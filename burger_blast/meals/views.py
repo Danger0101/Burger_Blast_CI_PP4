d880dc2e7392da3445119ac7492df1dc0e0c7773
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-14 15:12:08 +0000

    Added a check to see if user is super user
    This added a check to see if the user is a superuser or not for the fact super users can edit or create meals while normal user non super can view them as a normal menu.

--- burger_blast/meals/views.py
+++ burger_blast/meals/views.py
@@ -3,3 +3,28 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseForbidden
 from .models import Meal
 
+# Define a test function to check if the user is a superuser
+def is_superuser(user):
+    return user.is_superuser
+
+@user_passes_test(is_superuser)
+def create_or_update_meal(request, meal_id=None):
+    if meal_id:
+        meal = get_object_or_404(Meal, pk=meal_id)
+    else:
+        meal = Meal()
+
+    if request.method == 'POST':
+        meal.name = request.POST['name']
+        meal.description = request.POST['description']
+        meal.people = request.POST['people']
+        meal.price = request.POST['price']
+        meal.image = request.POST['image']
+        meal.calories = request.POST['calories']
+        meal.allergens = request.POST['allergens']
+        meal.preparation_time = request.POST['preparation_time']
+        meal.slug = request.POST['slug']
+
+
+    meal.save()
+    return render(request, 'menu_edit.html', {'meal': meal})
