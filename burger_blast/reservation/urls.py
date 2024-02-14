c7e002500318fb16da1b4910b6e6a3b58fe8a15f
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-14 15:44:11 +0000

    Added make reservations url

--- burger_blast/reservation/urls.py
+++ burger_blast/reservation/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = 'reservation'
 
 urlpatterns = [
+    path('make/', make_reservation, name='make_reservation'),
     path('my/', my_reservations, name='my_reservations'),
 ]
