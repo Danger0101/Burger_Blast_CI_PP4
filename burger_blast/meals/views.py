27599ff64620ed91441bfb2fce0a1ec2fd08a5ec
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-15 13:13:48 +0000

    Added Doc strings Removed images

--- burger_blast/meals/views.py
+++ burger_blast/meals/views.py
@@ -1,14 +1,31 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseForbidden
 from .models import Meal
 
-# Define a test function to check if the user is a superuser
 def is_superuser(user):
+    '''
+    Check if the user is a superuser.
+
+    Args:
+        user (User): The user to check if they are a superuser.
+
+    Returns:
+        bool: True if the user is a superuser, False otherwise.
+    '''
     return user.is_superuser
 
 @user_passes_test(is_superuser)
 def create_or_update_meal(request, meal_id=None):
+    '''
+    Create or update a meal.
+
+    Args:
+        request (HttpRequest): The request object.
+        meal_id (int, optional): The ID of the meal to update.
+
+    Returns:
+        HttpResponse: The response object.
+    '''
     if meal_id:
         meal = get_object_or_404(Meal, pk=meal_id)
     else:
@@ -19,21 +36,40 @@ def create_or_update_meal(request, meal_id=None):
         meal.description = request.POST['description']
         meal.people = request.POST['people']
         meal.price = request.POST['price']
-        meal.image = request.POST['image']
         meal.calories = request.POST['calories']
         meal.allergens = request.POST['allergens']
         meal.preparation_time = request.POST['preparation_time']
         meal.slug = request.POST['slug']
 
-
-    meal.save()
-    return render(request, 'menu_edit.html', {'meal': meal})
+        meal.save()
+        return render(request, 'menu_edit.html', {'meal': meal})
+    else:
+        return render(request, 'menu_edit.html', {'meal': meal})
 
 def menu_view(request):
+    '''
+    Display the menu.
+
+    Args:
+        request (HttpRequest): The request object.
+
+    Returns:
+        HttpResponse: The response object.
+    '''
     context = {'active_page': 'menu'}
     meals = Meal.objects.all()
     return render(request, 'meals/menu.html', {'meals': meals})
 
 def meal_detail(request, slug):
+    '''
+    Display details of a meal.
+
+    Args:
+        request (HttpRequest): The request object.
+        slug (str): The slug of the meal.
+
+    Returns:
+        HttpResponse: The response object.
+    '''
     meal = get_object_or_404(Meal, slug=slug)
     return render(request, 'meals/meal_detail.html', {'meal': meal})
