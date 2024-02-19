3c5308a76d549f22110c2a215dcfa7c0d339165f
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-19 13:58:07 +0000

    Add doc strings
    Removed unused import and add doc strings

--- burger_blast/about/views.py
+++ burger_blast/about/views.py
@@ -1,10 +1,20 @@
+'''
+This module contains the views for the about app.
+'''
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index_view(request):
+    '''
+    Renders the index page.
+    '''
     context = {'active_page': 'index'}
     return render(request, 'about/index.html', context)
 
+
 def about_view(request):
+    '''
+    Renders the about page.
+    '''
     context = {'active_page': 'about'}
     return render(request, 'about/about.html', context)
