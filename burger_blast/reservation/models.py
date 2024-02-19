11f5267f928795e8295dc2f4928ea14d104d15f0
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-19 14:01:08 +0000

    Add doc strings

--- burger_blast/reservation/models.py
+++ burger_blast/reservation/models.py
@@ -1,9 +1,44 @@
+'''
+The Reservation model is a simple model that
+stores information about a reservation.
+'''
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Reservation(models.Model):
+    '''
+    The Reservation model stores information about a reservation.
+
+    Attributes:
+        user (ForeignKey):
+            The user who made the reservation.
+        reservation_datetime (DateTimeField):
+            The date and time of the reservation.
+        confirmation_number (str):
+            The confirmation number for the reservation.
+        first_name (str):
+            The first name of the person making the reservation.
+        last_name (str):
+            The last name of the person making the reservation.
+        email (EmailField):
+            The email address of the person making the reservation.
+        party_size (PositiveIntegerField):
+            The number of people in the party.
+        phone_number (str):
+            The phone number of the person making the reservation.
+        special_requests (TextField):
+            Any special requests for the reservation.
+
+    Methods:
+        save:
+            Overrides the save method to generate a
+            confirmation number if one is not provided.
+        __str__:
+            Returns a string representation of the reservation.
+
+    '''
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     reservation_datetime = models.DateTimeField()
     confirmation_number = models.CharField(
