56f81ea3ba62620e1cabeb9d43f151d22c7008e7
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-19 14:01:20 +0000

    Cleaned up code and Add doc strings

--- burger_blast/reservation/views.py
+++ burger_blast/reservation/views.py
@@ -1,4 +1,8 @@
+'''
+This module contains views for the reservation app.
+'''
 from datetime import datetime
+from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
@@ -9,6 +13,9 @@ from .models import Reservation
 
 @login_required
 def my_reservations(request):
+    '''
+    This view displays a list of reservations for the logged-in user.
+    '''
     if request.user.is_staff:
         query = request.GET.get('q')
         if query:
@@ -26,16 +33,25 @@ def my_reservations(request):
 
     # Convert reservation datetime to the user's timezone
     for reservation in reservations:
-        reservation.reservation_datetime = timezone.localtime(reservation.reservation_datetime)
+        reservation.reservation_datetime = timezone.localtime(
+            reservation.reservation_datetime
+        )
 
     if not reservations:
-        reservations = None  # Set reservations to None if no reservations found
+        reservations = None
 
-    return render(request, 'reservation/my_reservations.html', {'reservations': reservations})
+    return render(
+        request,
+        'reservation/my_reservations.html',
+        {'reservations': reservations}
+    )
 
 
 @login_required
 def make_reservation(request):
+    '''
+    This view allows the user to make a reservation.
+    '''
     if request.method == 'POST':
         form = ReservationForm(request.POST, user=request.user)
         if form.is_valid():
@@ -43,7 +59,10 @@ def make_reservation(request):
             reservation.user = request.user
 
             # Combine date and time fields
-            reservation_datetime = datetime.combine(form.cleaned_data['reservation_date'], form.cleaned_data['reservation_time'])
+            reservation_datetime = datetime.combine(
+                form.cleaned_data['reservation_date'],
+                form.cleaned_data['reservation_time']
+            )
             # Make the datetime object timezone-aware
             reservation_datetime = timezone.make_aware(reservation_datetime)
             reservation.reservation_datetime = reservation_datetime
@@ -55,19 +74,26 @@ def make_reservation(request):
 
     return render(request, 'reservation/make_reservation.html', {'form': form})
 
+
 @login_required
 def edit_reservation(request, reservation_id):
+    '''
+    This view allows the user to edit a reservation.
+    '''
     reservation = get_object_or_404(Reservation, id=reservation_id)
     if not request.user.is_staff:
-        # If not a staff member, check if the reservation belongs to the current user
         if reservation.user != request.user:
-            raise PermissionDenied("You don't have permission to edit this reservation.")
+            raise PermissionDenied("You don't have permission to edit this "
+                                   "reservation.")
 
     if request.method == 'POST':
         form = ReservationForm(request.POST, instance=reservation)
         if form.is_valid():
             updated_reservation = form.save(commit=False)
-            reservation_datetime = datetime.combine(form.cleaned_data['reservation_date'], form.cleaned_data['reservation_time'])
+            reservation_datetime = datetime.combine(
+                form.cleaned_data['reservation_date'],
+                form.cleaned_data['reservation_time']
+            )
             # Make the datetime object timezone-aware
             reservation_datetime = timezone.make_aware(reservation_datetime)
             updated_reservation.reservation_datetime = reservation_datetime
@@ -78,15 +104,19 @@ def edit_reservation(request, reservation_id):
 
     return render(request, 'reservation/edit_reservation.html', {'form': form})
 
+
 @login_required
 def delete_reservation(request, reservation_id):
+    '''
+    This view allows the user to delete a reservation.
+    '''
     reservation = get_object_or_404(Reservation, pk=reservation_id)
 
     # Check if the logged-in user is a staff member
     if not request.user.is_staff:
-        # If not a staff member, check if the reservation belongs to the current user
         if reservation.user != request.user:
-            raise PermissionDenied("You don't have permission to delete this reservation.")
+            raise PermissionDenied("You don't have permission to delete this "
+                                   "reservation.")
 
     # Delete the reservation
     reservation.delete()
