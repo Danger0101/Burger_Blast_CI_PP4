563feebf500009104ccd1f7c202f7d80c8e74f1b
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-19 13:59:21 +0000

    Add doc strings

--- burger_blast/meals/apps.py
+++ burger_blast/meals/apps.py
@@ -1,6 +1,12 @@
+'''
+This file is used to configure the app name for the meals app
+'''
 from django.apps import AppConfig
 
 
 class MealsConfig(AppConfig):
+    '''
+    Configuration for the meals app.
+    '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'meals'
