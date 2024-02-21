3a9ade09429c3a5da16c82cd67475c2e68856b1b
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-21 21:01:08 +0000

    Cleaned up the layout of urls.py

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,11 +1,24 @@
 from django.urls import path
-from .views import *
+from .views import (
+    make_reservation,
+    my_reservations,
+    edit_reservation,
+    delete_reservation
+)
 
 app_name = 'reservation'
 
 urlpatterns = [
     path('make/', make_reservation, name='make_reservation'),
     path('my/', my_reservations, name='my_reservations'),
-    path('<int:reservation_id>/edit/', edit_reservation, name='edit_reservation'),
-    path('<int:reservation_id>/delete/', delete_reservation, name='delete_reservation'),
+    path(
+        '<int:reservation_id>/edit/',
+        edit_reservation,
+        name='edit_reservation'
+    ),
+    path(
+        '<int:reservation_id>/delete/',
+        delete_reservation,
+        name='delete_reservation'
+    ),
 ]
