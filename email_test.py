6e4fc3e748f2567291b4b0933b64d0e78bd43522
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-21 20:57:57 +0000

    Initial Commit
    Was able to successfully send a sendgrid email with this automated test.

---
+++ email_test.py
@@ -0,0 +1,18 @@
+import os
+import django
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Set the Django settings module dynamically
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'burger_blast.settings')
+django.setup()
+
+# Send test email
+send_mail(
+    'Testing email',
+    'This is a test email from Burger Blast project.',
+    settings.DEFAULT_FROM_EMAIL,
+    ['jhummel001@gmail.com'],
+)
+
+# Works in development environment
