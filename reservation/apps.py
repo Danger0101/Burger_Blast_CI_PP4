7f6ea75530b4c6d74eae75333dc80cef88b8374a
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-22 20:12:49 +0000

    Add doc strings

--- reservation/apps.py
+++ reservation/apps.py
@@ -1,6 +1,12 @@
+'''
+The apps.py file is used to configure the reservation app.
+'''
 from django.apps import AppConfig
 
 
 class ReservationConfig(AppConfig):
+    '''
+    The ReservationConfig class is used to configure the reservation app.
+    '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reservation'
