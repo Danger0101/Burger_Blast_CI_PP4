e6de95c1cfe9ee4530619a46061c440f1ba4deed
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-22 20:13:33 +0000

    Add docstring

--- meals/admin.py
+++ meals/admin.py
@@ -1,3 +1,7 @@
+'''
+This file is used to register the Meal model with the Django admin
+so that we can manage the meals from the admin interface.
+'''
 from django.contrib import admin
 from .models import Meal
 
