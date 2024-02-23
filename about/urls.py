40dea48fe9c9596831c6fdc74ba3696d8b423ba4
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-23 13:25:14 +0000

    Added contact us to urls

--- about/urls.py
+++ about/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index_view, about_view  # Import the about_view
+from .views import index_view, about_view, contact_us
 
 app_name = 'about'
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('about/', about_view, name='about'),
+    path('contact-us/', contact_us, name='contact_us'),
 ]
