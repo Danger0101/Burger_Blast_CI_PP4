c585b970e3f35e396e8c9f37aa63fe6c57936d09
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-25 10:30:04 +0000

    Create a menu remove test case
     python manage.py test meals.test_menu_remove
    ----
    Found 1 test(s).
    Creating test database for alias 'default'...
    System check identified no issues (0 silenced).
    Added Entree to the menu.
    Added Side Dishes to the menu.
    Added Desserts to the menu.
    D:\GitHub\Burger_Blast_CI_PP4\venv\Lib\site-packages\admin_interface\templatetags\admin_interface_tags.py:38: UserWarning: Language chooser requires Django's `set_language` view: `urlpatterns += [url(r'^i18n/', include('django.conf.urls.i18n'))]`.
      warnings.warn(
    Removed Entree from the menu.
    Removed Side Dishes from the menu.
    Removed Desserts from the menu.
    .
    -------------
    Ran 1 test in 1.128s
    
    OK
    Destroying test database for alias 'default'...

---
+++ meals/test_menu_remove.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import User
+from django.contrib.admin.sites import AdminSite
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Meal
+from .admin import MealAdmin
+
+
+class AdminRemoveItemTestCase(TestCase):
+    '''
+    This test case is used to remove items from the menu using the admin interface.
+    '''
+    def setUp(self):
+        # Create a superuser
+        self.user = User.objects.create_superuser(
+            username='admin',
+            email='admin@example.com',
+            password='adminpassword'
+        )
+
+        # Initialize the client and log in as the superuser
+        self.client = Client()
+        self.client.login(username='admin', password='adminpassword')
+
+        # Add items to the menu
+        categories = ['Entree', 'Side Dishes', 'Desserts']
+        for category in categories:
+            meal_data = {
+                'name': f'Test Meal {category}',
+                'description': f'Test description for {category}',
+                'people': 1,
+                'price': 9.99,
+                'calories': 500,
+                'allergens': 'Test Allergens',
+                'preparation_time': 20,
+                'slug': f'test-{category.lower()}',
+                'category': category
+            }
+            # Add the item to the menu
+            self.client.post(reverse('admin:meals_meal_add'), meal_data)
+            print(f"Added {category} to the menu.")
+
+    def test_remove_items(self):
+        # Remove items from each category
+        categories = ['Entree', 'Side Dishes', 'Desserts']
+        for category in categories:
+            meal = Meal.objects.get(name=f'Test Meal {category}')
+            response_delete = self.client.post(reverse('admin:meals_meal_delete', args=(meal.id,)))
+            print(f"Removed {category} from the menu.")
+
+        # Register the Meal model with the custom admin site
+        admin_site = AdminSite()
+        admin_site.register(Meal, MealAdmin)
