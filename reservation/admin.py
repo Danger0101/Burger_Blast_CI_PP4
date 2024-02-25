fd38d598925c0b20fbc9afba07ceaf9f8d0050ff
John Hummel <34178990+Danger0101@users.noreply.github.com>
2024-02-25 09:31:16 +0000

    Added better layout to admin site for reservations

--- reservation/admin.py
+++ reservation/admin.py
@@ -4,4 +4,21 @@ This file is used to register the Reservation model with the admin site.
 from django.contrib import admin
 from .models import Reservation
 
-admin.site.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'reservation_datetime', 'confirmation_number', 'party_size', 'phone_number')  
+    list_filter = ('reservation_datetime', 'party_size')  
+    search_fields = ('user__username', 'first_name', 'last_name', 'email', 'phone_number')  
+    readonly_fields = ('confirmation_number',)
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'reservation_datetime', 'confirmation_number')
+        }),
+        ('Personal Information', {
+            'fields': ('first_name', 'last_name', 'email', 'phone_number')
+        }),
+        ('Reservation Details', {
+            'fields': ('party_size', 'special_requests'),
+        }),
+    )
+
+admin.site.register(Reservation, ReservationAdmin)
